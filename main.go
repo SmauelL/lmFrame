0c506f6a3a20d0749031b3aceaad1dd22776d431
SmauelL <ljxlm502661058@163.com>
2020-09-09 16:02:50 +0800

    context and router

--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"lm"
 	"net/http"
 )
 
 func main() {
 	r := lm.New()
-	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+
+	r.GET("/", func(c *lm.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Lm</h1>")
 	})
-	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
-		for k, v := range req.Header {
-			fmt.Fprintf(w, "Header [%q]= %q\n", k, v)
-		}
+
+	r.GET("/hello", func(c *lm.Context) {
+		// expect /hello?name=lm
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
+
+	r.POST("/login", func(c *lm.Context) {
+		c.JSON(http.StatusOK, lm.S{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
+
 	r.Run(":9999")
 }
