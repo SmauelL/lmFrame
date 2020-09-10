8f6ed665db99d8e6cdb2f37a662bf7807dd1947e
SmauelL <ljxlm502661058@163.com>
2020-09-10 15:32:28 +0800

    group

--- main.go
+++ main.go
@@ -8,22 +8,34 @@ import (
 func main() {
 	r := lm.New()
 
-	r.GET("/", func(c *lm.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Lm</h1>")
+	r.GET("/index", func(c *lm.Context) {
+		c.HTML(http.StatusOK, "<h1>index</h1>")
 	})
 
-	r.GET("/hello", func(c *lm.Context) {
-		// expect /hello?name=lm
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
-	r.GET("/hello/:name", func(c *lm.Context) {
-		//expect /hello/lm
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
+	v1 := r.Group("v1")
+	{
+		v1.GET("/", func(c *lm.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello lm</h1>")
+		})
 
-	r.GET("/assets/*filepath", func(c *lm.Context) {
-		c.JSON(http.StatusOK, lm.S{"filepath": c.Param("filepath")})
-	})
+		v1.GET("/hello", func(c *lm.Context) {
+			// expect /hello?name=lm
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("v2")
+	{
+		v2.GET("/hello/:name", func(c *lm.Context) {
+			//expect /hello/lm
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *lm.Context) {
+			c.JSON(http.StatusOK, lm.S{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+	}
 
 	r.Run(":9999")
 }
